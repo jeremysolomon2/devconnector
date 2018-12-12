3c4b1eefbf4295092cacc4002f4ad4f714772ead
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-12-12 16:08:57 -0500

    profile validation in progress

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile')
+
 // Load Profile Model
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -38,6 +41,15 @@ router.get('/',
 router.post('/',
     passport.authenticate('jwt', {session: false }),
     (req, res) => {
+        const { errors, isValid } = validateProfileInput(req.body);
+
+        // Check Validation
+        if(!isValid) {
+            // Return any errors with 400 status
+            return res.status(400).json(errors);
+        }
+
+
         // Get fields
         const profileFields = {};
         profileFields.user = req.user.id;
