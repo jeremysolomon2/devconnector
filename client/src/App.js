72edcbfbee112655d761a1e4010bcdc230c2636f
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-12-22 13:57:55 -0500

    broken on client side Register.js file.  onChange function

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-        <Router>
-          <div className="App">
-            <Navbar/>
-            <Route exact path="/" component={Landing} />
-            <div className="container">
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-            </div>
-            <Footer/>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
           </div>
-        </Router>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
