d94dfd2a7e9e13ff15def4b7383bbe58b449e89d
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-12-24 11:03:23 -0500

    reduc action and reducer

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
 import classnames from "classnames";
 import { connect } from "react-redux";
@@ -41,8 +42,11 @@ class Register extends Component {
   render() {
     const { errors } = this.state;
 
+    const { user } = this.props.auth;
+
     return (
       <div className="register">
+        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -125,7 +129,16 @@ class Register extends Component {
   }
 }
 
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { registerUser }
 )(Register);
